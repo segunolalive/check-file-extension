fcd2071d14ea2a2a98f6f24c4f91b739ba21f5b0
segun ola <segunlalive@gmail.com>
2017-07-24 11:45:53 +0100

    added assertion testing

--- test.js
+++ test.js
@@ -1,3 +1,12 @@
+const assert = require('assert');
 const cfe = require('./cfe.js');
 
-console.log(cfe('a/w/e/s/o/me/file.txt'));
+
+function test(description, fn, file, expected) {
+  console.log("TEST: ", description);
+  let actual = fn(file);
+  assert.equal(actual, expected, `Expected ${expected} \nGot ${actual} instead}`);
+  console.log("passed test");
+}
+
+test('returns file extension', cfe, 'a/w/e/s/o/me/file.txt', '.txt');
